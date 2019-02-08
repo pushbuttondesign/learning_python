ef4245079e7c80e1dcca7291a841e43c5ea35dac
chewdonkey <chewdonkey@gmail.com>
2019-02-08 22:17:17 +0000

    added typehelper

---
+++ typehelper.py
@@ -0,0 +1,8 @@
+import typing
+string = typing.TypeVar(str);
+
+def func(var: str, var2: int) -> typing.List[int, str]:
+    var3: list[str, int] = [var, var];
+    return 0;
+
+print(func("test", 1));
